7656f62076dc69ae7bffce2bb8a00567d027d12e
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-10-26 20:59:36 +0700

    Initial Commit

---
+++ Twitter-clone/Twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_cloneApp.swift
+//  Twitter-clone
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
