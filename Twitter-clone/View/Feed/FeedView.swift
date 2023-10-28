9c9e0c6ee9ec672ff4007c00169d6b99d0aa7605
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-10-28 10:21:38 +0700

    FeedView

--- Twitter-clone/View/Feed/FeedView.swift
+++ Twitter-clone/View/Feed/FeedView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed")
+        ScrollView(.vertical,showsIndicators: false){
+            VStack(spacing: 18){
+                TweetCellView(tweet: "Matcha laf cute soos motoj thees giowis", tweetImage: "post")
+                Divider()
+                ForEach(1...20, id: \.self){ _ in
+                    TweetCellView(tweet: sampleText)
+                    Divider()
+                }
+            }
+            .padding(.top)
+            .padding(.horizontal)
+            .zIndex(0)
+        }
     }
 }
 
---
+++ Twitter-clone/View/Feed/TweetCellView.swift
@@ -0,0 +1,89 @@
+//
+//  TweetCellView.swift
+//  Twitter-clone
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 27/10/2023.
+//
+
+import SwiftUI
+
+struct TweetCellView: View {
+    var tweet: String
+    var tweetImage: String?
+    var body: some View {
+        VStack{
+            HStack(alignment: .top,spacing: 10) {
+               Image("logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 55, height: 55)
+                    .clipShape(Circle())
+                VStack(alignment: .leading, spacing: 10){
+                    (Text("Cem")
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                    +
+                    Text("@cem_salta")
+                    )
+                    Text(tweet)
+                        .frame(maxHeight: 100, alignment: .top)
+                    if let tweetImage = tweetImage {
+                        GeometryReader { proxy in
+                            Image(tweetImage)
+                                .resizable()
+                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                .frame(width: proxy.frame(in: .global).width, height: 250)
+                                .cornerRadius(15)
+                        }
+                        .frame(height: 250)
+                    }
+                    
+                }
+            }
+            //Cell Bottom
+            HStack(spacing: 50, content: {
+                Button{
+                    
+                }label: {
+                    Image("Comments")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                    
+                }.foregroundColor(.gray)
+                Button{
+                    
+                }label: {
+                    Image("Retweet")
+                        .resizable()
+                        .frame(width: 18, height: 16)
+                    
+                }.foregroundColor(.gray)
+                Button{
+                    
+                }label: {
+                    Image("love")
+                        .resizable()
+                        .frame(width: 18, height: 15)
+                    
+                }.foregroundColor(.gray)
+                Button{
+                    
+                }label: {
+                    Image("upload")
+                        .resizable()
+                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                        .frame(width: 18, height: 16)
+                    
+                }.foregroundColor(.gray)
+            })
+            .padding(.top, 4)
+            
+        }
+    }
+}
+
+#Preview {
+    TweetCellView(tweet: sampleText)
+}
+
+ var sampleText = "Loren ipsun dolor sit anet, consectetur adipisci elit, sed eiusnod tenpor incidunt ut labore et dolore nagna aliqua. Ut enin ad ninin venian, quis nostrun exercitationen ullan corporis suscipit laboriosan, nisi ut aliquid ex ea connodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillun dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt nollit anin id est laborun."
