bb9e20425de3446595bbc2d3d52b0f8e860862ad
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-11-03 16:02:14 +0700

    RegisterView

---
+++ Twitter-clone/View/Authentication/RegisterView.swift
@@ -0,0 +1,69 @@
+//
+//  RegisterView.swift
+//  Twitter-clone
+//
+//  Created by Duy Huỳnh Nguyễn Tuấn on 03/11/2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    var body: some View {
+        VStack{
+            ZStack{
+                HStack{
+                    Button{
+                        
+                    }label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                }.padding(.horizontal)
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+                
+            }
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 30)
+            VStack(alignment: .leading){
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+            }
+            Spacer()
+            VStack{
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                HStack{
+                    Spacer()
+                    Button{
+                        
+                    }label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundColor(Color("bg"))
+                            .overlay(
+                            Text("Next")
+                                .foregroundColor(.white)
+                            )
+                    }
+                }
+                .padding(.trailing, 24)
+            }
+        }
+    }
+}
+
+#Preview {
+    RegisterView()
+}
--- Twitter-clone/View/Authentication/WelcomeView.swift
+++ Twitter-clone/View/Authentication/WelcomeView.swift
@@ -9,111 +9,114 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        VStack{
-            HStack{
-                Spacer()
-                Image("Twitter")
-                    .resizable()
-                    .scaledToFill()
-                    .padding(.trailing)
-                    .frame(width: 20, height: 20)
-                Spacer()
-            }
-            Spacer()
-            Text("See what's happening in the world right now.")
-                .font(.system(size: 30, weight: .heavy, design: .default))
-                .frame(width: getRect().width * 0.9,alignment: .center)
-            Spacer()
-            VStack(spacing: 10){
-                Button{
-                   print("google signin")
-                }label: {
-                    HStack(spacing: -4){
-                        Image("google")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 25, height: 25)
-                        Text("Continue with Google")
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                    .overlay(
-                    RoundedRectangle(cornerRadius: 36)
-                        .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                        .opacity(0.3)
-                        .frame(width: 320, height: 60, alignment: .center)
-                    )
-                }
-                Button{
-                    
-                }label: {
-                    HStack(spacing: -4){
-                        Image("apple")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 25, height: 25)
-                        Text("Continue with Apple")
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .font(.title3)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                    .overlay(
-                    RoundedRectangle(cornerRadius: 36)
-                        .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                        .opacity(0.3)
-                        .frame(width: 320, height: 60, alignment: .center)
-                    )
-                }
+        NavigationView{
+            VStack{
                 HStack{
-                    Rectangle()
-                        .foregroundColor(.gray)
-                        .opacity(0.3)
-                        .frame(width: getRect().width * 0.35, height: 1)
-                    Text("Or")
-                        .foregroundColor(.gray)
-                    Rectangle()
-                        .foregroundColor(.gray)
-                        .opacity(0.3)
-                        .frame(width: getRect().width * 0.35, height: 1)
+                    Spacer()
+                    Image("Twitter")
+                        .resizable()
+                        .scaledToFill()
+                        .padding(.trailing)
+                        .frame(width: 20, height: 20)
+                    Spacer()
                 }
-                RoundedRectangle(cornerRadius: 36)
-                    .foregroundColor(Color("bg"))
-                    .frame(width: 320, height: 60, alignment: .center)
-                    .overlay(
-                    Text("Create account")
-                        .fontWeight(.bold)
-                        .font(.title3)
-                        .foregroundColor(.white)
-                        .padding()
-                    )
-            }
-            .padding()
-            VStack(alignment: .leading){
-                VStack{
-                    Text("By proceeding, you agree to our ")
-                    +
-                    Text("Term")
+                Spacer()
+                Text("See what's happening in the world right now.")
+                    .font(.system(size: 30, weight: .heavy, design: .default))
+                    .frame(width: getRect().width * 0.9,alignment: .center)
+                Spacer()
+                VStack(spacing: 10){
+                    Button{
+                       print("google signin")
+                    }label: {
+                        HStack(spacing: -4){
+                            Image("google")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                            Text("Continue with Google")
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 36)
+                            .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                            .opacity(0.3)
+                            .frame(width: 320, height: 60, alignment: .center)
+                        )
+                    }
+                    Button{
+                        
+                    }label: {
+                        HStack(spacing: -4){
+                            Image("apple")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                            Text("Continue with Apple")
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                        .overlay(
+                        RoundedRectangle(cornerRadius: 36)
+                            .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                            .opacity(0.3)
+                            .frame(width: 320, height: 60, alignment: .center)
+                        )
+                    }
+                    HStack{
+                        Rectangle()
+                            .foregroundColor(.gray)
+                            .opacity(0.3)
+                            .frame(width: getRect().width * 0.35, height: 1)
+                        Text("Or")
+                            .foregroundColor(.gray)
+                        Rectangle()
+                            .foregroundColor(.gray)
+                            .opacity(0.3)
+                            .frame(width: getRect().width * 0.35, height: 1)
+                    }
+                    RoundedRectangle(cornerRadius: 36)
                         .foregroundColor(Color("bg"))
-                    +
-                    Text(",")
-                    +
-                    Text("Privacy Policy")
-                        .foregroundColor(Color("bg"))
-                    +
-                    Text(", and ")
-                    +
-                    Text("Cookie Use")
-                        .foregroundStyle(Color("bg"))
-                }.padding(.bottom)
-                HStack(spacing: 2){
-                    Text("Have an account already?")
-                    Text("Log in")
-                        .foregroundColor(Color("bg"))
+                        .frame(width: 320, height: 60, alignment: .center)
+                        .overlay(
+                        Text("Create account")
+                            .fontWeight(.bold)
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .padding()
+                        )
                 }
-            }
+                .padding()
+                VStack(alignment: .leading){
+                    VStack{
+                        Text("By proceeding, you agree to our ")
+                        +
+                        Text("Term")
+                            .foregroundColor(Color("bg"))
+                        +
+                        Text(",")
+                        +
+                        Text("Privacy Policy")
+                            .foregroundColor(Color("bg"))
+                        +
+                        Text(", and ")
+                        +
+                        Text("Cookie Use")
+                            .foregroundStyle(Color("bg"))
+                    }.padding(.bottom)
+                    HStack(spacing: 2){
+                        Text("Have an account already?")
+                        Text("Log in")
+                            .foregroundColor(Color("bg"))
+                    }
+                }
+            }.navigationTitle("")
+            .navigationBarHidden(true)
         }
     }
 }
