db97d1ccf0636c8c4a6a2b5af76b79904a310284
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-11-03 15:12:33 +0700

    welcome view

--- Twitter-clone/View/Authentication/WelcomeView.swift
+++ Twitter-clone/View/Authentication/WelcomeView.swift
@@ -25,7 +25,94 @@ struct WelcomeView: View {
                 .frame(width: getRect().width * 0.9,alignment: .center)
             Spacer()
             VStack(spacing: 10){
-                
+                Button{
+                   print("google signin")
+                }label: {
+                    HStack(spacing: -4){
+                        Image("google")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                        Text("Continue with Google")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .padding()
+                    }
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 36)
+                        .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                        .opacity(0.3)
+                        .frame(width: 320, height: 60, alignment: .center)
+                    )
+                }
+                Button{
+                    
+                }label: {
+                    HStack(spacing: -4){
+                        Image("apple")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                        Text("Continue with Apple")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .padding()
+                    }
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 36)
+                        .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                        .opacity(0.3)
+                        .frame(width: 320, height: 60, alignment: .center)
+                    )
+                }
+                HStack{
+                    Rectangle()
+                        .foregroundColor(.gray)
+                        .opacity(0.3)
+                        .frame(width: getRect().width * 0.35, height: 1)
+                    Text("Or")
+                        .foregroundColor(.gray)
+                    Rectangle()
+                        .foregroundColor(.gray)
+                        .opacity(0.3)
+                        .frame(width: getRect().width * 0.35, height: 1)
+                }
+                RoundedRectangle(cornerRadius: 36)
+                    .foregroundColor(Color("bg"))
+                    .frame(width: 320, height: 60, alignment: .center)
+                    .overlay(
+                    Text("Create account")
+                        .fontWeight(.bold)
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .padding()
+                    )
+            }
+            .padding()
+            VStack(alignment: .leading){
+                VStack{
+                    Text("By proceeding, you agree to our ")
+                    +
+                    Text("Term")
+                        .foregroundColor(Color("bg"))
+                    +
+                    Text(",")
+                    +
+                    Text("Privacy Policy")
+                        .foregroundColor(Color("bg"))
+                    +
+                    Text(", and ")
+                    +
+                    Text("Cookie Use")
+                        .foregroundStyle(Color("bg"))
+                }.padding(.bottom)
+                HStack(spacing: 2){
+                    Text("Have an account already?")
+                    Text("Log in")
+                        .foregroundColor(Color("bg"))
+                }
             }
         }
     }
