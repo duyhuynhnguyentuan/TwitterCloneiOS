cb8b9af25ef753ffcabbc8f0381e3282d6a21200
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-11-24 21:26:51 +0700

    ImagePicker

--- Twitter-clone/View/Authentication/ContentView.swift
+++ Twitter-clone/View/Authentication/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         if viewModel.isAuthenticated  {
             if let user = viewModel.currentUser {
-                    MainView(user: user)
+                     MainView(user: user)
             }
         }else {
             WelcomeView()
