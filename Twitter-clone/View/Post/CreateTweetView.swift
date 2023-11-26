a130155027066b7702a2cf4f560152f05c2ccb67
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-11-26 23:36:37 +0700

    Image picker represented

--- Twitter-clone/View/Post/CreateTweetView.swift
+++ Twitter-clone/View/Post/CreateTweetView.swift
@@ -11,6 +11,9 @@ struct CreateTweetView: View {
     @State var text = ""
     @Binding var show : Bool
     @ObservedObject var viewModel = CreateTweetViewModel()
+    @State var imagePickerPresented = false
+    @State var selectedImage: UIImage?
+    @State var postImage: Image?
     var body: some View {
         VStack{
             HStack{
@@ -37,9 +40,47 @@ struct CreateTweetView: View {
                 .clipShape(Capsule())
             }
             MultilineTextField(text: $text)
-            
+            if postImage == nil {
+                Button{
+                    imagePickerPresented.toggle()
+                }label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 70, height: 70)
+                        .clipped()
+                        .padding(.top)
+                        .foregroundColor(.black)
+
+                }.sheet(isPresented: $imagePickerPresented){
+                    loadImage()
+                } content: {
+                    ImagePicker(image: $selectedImage)
+                }
+            }
+           else if let image = postImage {
+                VStack {
+                    HStack(alignment: .top){
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .padding(.horizontal)
+                            .frame(width: UIScreen.main.bounds.width * 0.9)
+                            .cornerRadius(16)
+                            .clipped()
+                    }
+                    Spacer()
+                }
+            }
         }
         .padding()
+    }
+}
+
+extension CreateTweetView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        postImage = Image(uiImage: selectedImage)
     }
 }
 
